4f46eb05a91f54b5a815b0c7d94c73bebb5ec4e8
Shouduo <handsomesaywhynot@gmail.com>
2016-10-24 17:25:30 +0800

    Code refactoring.

--- app/src/main/java/com/shouduo/choosepictest/MainActivity.java
+++ app/src/main/java/com/shouduo/choosepictest/MainActivity.java
@@ -2,7 +2,6 @@ package com.shouduo.choosepictest;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -11,19 +10,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final int TAKE_PHOTO = 1;
-    public static final int CROP_PHOTO = 2;
+    private static final String IMAGE_FILE_NAME = "temp_head_image.jpg";
 
-    private Button takePhoto;
-    private Button chooseFromAlbum;
-    private ImageView picture;
+    private static final int CODE_GALLERY_REQUEST = 0xa0;
+    private static final int CODE_CAMERA_REQUEST = 0xa1;
+    private static final int CODE_RESULT_REQUEST = 0xa2;
+
+    private static int output_X = 600;
+    private static int output_Y = 600;
+
+    private Button buttonCamera;
+    private Button buttonLocal;
+    private ImageView headImage;
     private Uri imageUri;
 
     @Override
@@ -31,77 +38,152 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        takePhoto = (Button) findViewById(R.id.take_photo);
-        picture = (ImageView) findViewById(R.id.picture);
-        takePhoto.setOnClickListener(new View.OnClickListener() {
+        headImage = (ImageView) findViewById(R.id.headImage);
+        buttonLocal = (Button) findViewById(R.id.buttonLocal);
+        buttonLocal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                File outputImage = new File(Environment.getExternalStorageDirectory(), "output_image");
-                try {
-                    if (outputImage.exists()) {
-                        outputImage.delete();
-                    }
-                    outputImage.createNewFile();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                imageUri = Uri.fromFile(outputImage);
-                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-                intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
-                startActivityForResult(intent, TAKE_PHOTO);
+                chooseHeadImageFromGallery();
             }
         });
 
-        chooseFromAlbum = (Button) findViewById(R.id.choose_from_album);
-        chooseFromAlbum.setOnClickListener(new View.OnClickListener() {
+        buttonCamera = (Button) findViewById(R.id.buttonCamera);
+        buttonCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                File outputImage = new File(Environment.getExternalStorageDirectory(), "output_image.jpg");
-                try {
-                    if (outputImage.exists()) {
-                        outputImage.delete();
-                    }
-                    outputImage.createNewFile();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                imageUri = Uri.fromFile(outputImage);
-                Intent intent = new Intent("android.intent.action.GET_CONTENT");
-                intent.setType("image/*");
-                intent.putExtra("crop", true);  //Something not right here
-                intent.putExtra("scale", true);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-                startActivityForResult(intent, CROP_PHOTO);
+                chooseHeadImageFromCameraCapture();
             }
         });
+    }
+
+    private void chooseHeadImageFromGallery() {
+        Intent intentFromGallery = new Intent();
+        intentFromGallery.setType("image/*");
+        intentFromGallery.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intentFromGallery, CODE_GALLERY_REQUEST);
+    }
+
+    private void chooseHeadImageFromCameraCapture() {
+        Intent intentFromCapture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (hasSdcard()) {
+            intentFromCapture.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(
+                    new File(Environment.getExternalStorageDirectory(), IMAGE_FILE_NAME)));
+        }
+        startActivityForResult(intentFromCapture, CODE_CAMERA_REQUEST);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        // 用户没有进行有效的设置操作，返回
+        if (resultCode == RESULT_CANCELED) {//取消
+            Toast.makeText(getApplication(), "Canceled", Toast.LENGTH_LONG).show();
+            return;
+        }
+
         switch (requestCode) {
-            case TAKE_PHOTO:
-                if (resultCode == RESULT_OK) {
-                    Intent intent = new Intent("com.android.camera.action.CROP");
-                    intent.setDataAndType(imageUri, "image/*");
-                    intent.putExtra("scale", true);
-                    intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-                    startActivityForResult(intent, CROP_PHOTO);
-                }
+            case CODE_GALLERY_REQUEST://如果是来自本地的
+                cropRawPhoto(intent.getData());//直接裁剪图片
                 break;
-            case CROP_PHOTO:
-                if (resultCode == RESULT_OK) {
-                    try {
-                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
-                        picture.setImageBitmap(bitmap);
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    }
+
+            case CODE_CAMERA_REQUEST:
+                if (hasSdcard()) {
+                    File tempFile = new File(
+                            Environment.getExternalStorageDirectory(),
+                            IMAGE_FILE_NAME);
+                    cropRawPhoto(Uri.fromFile(tempFile));
+                } else {
+                    Toast.makeText(getApplication(), "没有SDCard!", Toast.LENGTH_LONG)
+                            .show();
                 }
+
                 break;
-            default:
+
+            case CODE_RESULT_REQUEST:
+                if (intent != null) {
+                    setImageToHeadView(intent);//设置图片框
+                }
+
                 break;
         }
+
+        super.onActivityResult(requestCode, resultCode, intent);
     }
+
+    /**
+     * 裁剪原始的图片
+     */
+    public void cropRawPhoto(Uri uri) {
+
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+
+        //把裁剪的数据填入里面
+
+        // 设置裁剪
+        intent.putExtra("crop", "true");
+
+        // aspectX , aspectY :宽高的比例
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+
+        // outputX , outputY : 裁剪图片宽高
+        intent.putExtra("outputX", output_X);
+        intent.putExtra("outputY", output_Y);
+        intent.putExtra("return-data", true);
+
+        startActivityForResult(intent, CODE_RESULT_REQUEST);
+    }
+
+    /**
+     * 提取保存裁剪之后的图片数据，并设置头像部分的View
+     */
+    private void setImageToHeadView(Intent intent) {
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            Bitmap photo = extras.getParcelable("data");
+            headImage.setImageBitmap(photo);
+
+            //新建文件夹 先选好路径 再调用mkdir函数 现在是根目录下面的Ask文件夹
+            File nf = new File(Environment.getExternalStorageDirectory() + "/Ask");
+            nf.mkdir();
+
+            //在根目录下面的ASk文件夹下 创建okkk.jpg文件
+            File f = new File(Environment.getExternalStorageDirectory() + "/Ask", "okkk.jpg");
+
+            FileOutputStream out = null;
+            try {
+
+                //打开输出流 将图片数据填入文件中
+                out = new FileOutputStream(f);
+                photo.compress(Bitmap.CompressFormat.PNG, 90, out);
+
+                try {
+                    out.flush();
+                    out.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+
+    public static boolean hasSdcard() {
+        String state = Environment.getExternalStorageState();
+        if (state.equals(Environment.MEDIA_MOUNTED)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 }
 
 
